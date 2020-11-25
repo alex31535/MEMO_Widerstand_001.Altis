ed63f87f7f6644b90fe7115a2019a8409fc64f26
alex31535 <alex31535@miegelke.de>
2020-11-25 18:49:24 +0100

    Create s_feindausstattung.hpp
    WIP
    level-bezogener waffenliste zur ausstattung von feind-ki

---
+++ s_feindausstattung.hpp
@@ -0,0 +1,107 @@
+
+s_feindausstattung = [
+
+[/* lvl 0 */
+/* pistolen */
+[2,"CUP_hgun_Compact",false],
+[2,"CUP_hgun_Duty",false],
+[2,"CUP_hgun_Phantom",false],
+[2,"CUP_hgun_Glock17",false],
+[2,"gm_lp1_blk",false],
+[2,"CUP_hgun_Colt1911",false],
+[2,"CUP_hgun_M9",false],
+[2,"CUP_hgun_Makarov",false],
+[2,"CUP_hgun_MicroUzi",false],
+[2,"hgun_P07_khk_F",false],
+[2,"hgun_P07_blk_F",false],
+[2,"gm_p1_blk",false],
+[2,"gm_p2a1_blk",false],
+[2,"CUP_hgun_PB6P9",false],
+[2,"hgun_Pistol_01_F",false],
+[2,"gm_pm_blk",false],
+[2,"CUP_hgun_SA61",false],
+[2,"CUP_hgun_TaurusTracker455",false],
+[2,"CUP_hgun_TaurusTracker455_gold",false],
+[2,"gm_wz78_blk",false],
+/* sturmgewehre */
+[0,"gm_mpiak74n_brn",false],
+[0,"gm_mpiak74n_prp",false],
+[0,"gm_mpiaks74n_brn",false],
+[0,"gm_mpiaks74n_prp",false],
+[0,"gm_mpiaks74nk_brn",false],
+[0,"gm_mpiaks74nk_prp",false],
+[0,"gm_mpikm72_brn",false],
+[0,"gm_mpikm72_prp",false],
+[0,"gm_mpikms72_brn",false],
+[0,"gm_mpikms72_prp",false]
+/* maschinengewehre */
+
+/* schrotgewehre */
+
+],
+[/* lvl 1 */
+/* pistolen */
+
+/* sturmgewehre */
+[0,"arifle_AK12_F",false],
+[0,"arifle_AK12_GL_F",false],
+[0,"CUP_arifle_AK107",false],
+[0,"CUP_arifle_AK107_GL",false],
+[0,"CUP_arifle_AK47",false],
+[0,"CUP_arifle_AK74",false],
+[0,"CUP_arifle_AK74_GL",false],
+[0,"CUP_arifle_AK74M",false],
+[0,"CUP_arifle_AK74M_GL",false],
+[0,"CUP_arifle_AKM",false],
+[0,"arifle_AKM_F",false],
+[0,"gm_akm_wud",false],
+[0,"gm_akmn_wud",false],
+[0,"CUP_arifle_AKS",false],
+[0,"CUP_arifle_AKS_Gold",false],
+[0,"arifle_AKS_F",false],
+[0,"CUP_arifle_AKS74",false],
+[0,"CUP_arifle_AKS74U",false],
+[0,"gm_c7a1_oli",false],
+[0,"gm_c7a1_blk",false],
+[0,"CUP_arifle_FNFAL5061",false],
+[0,"CUP_arifle_FNFAL5062",false],
+[0,"CUP_arifle_FNFAL_railed",false],
+[0,"CUP_arifle_FNFAL_OSW",false],
+[0,"CUP_arifle_FNFAL",false],
+[0,"gm_g3a3_oli",false],
+[0,"gm_g3a3_grn",false],
+[0,"gm_g3a3_blk",false],
+[0,"gm_g3a3_des",false],
+[0,"gm_g3a4_oli",false],
+[0,"gm_g3a4_grn",false],
+[0,"gm_g3a4_blk",false],
+[0,"gm_g3a4_des",false],
+[0,"gm_gvm75_oli",false],
+[0,"gm_gvm75_grn",false],
+[0,"gm_gvm75_blk",false],
+[0,"gm_gvm75carb_oli",false],
+[0,"gm_gvm75carb_grn",false],
+[0,"gm_gvm75carb_blk",false],
+[0,"gm_gvm95_blk",false],
+[0,"gm_m16a1_blk",false],
+[0,"CUP_arifle_M16A2",false],
+[0,"gm_m16a2_blk",false],
+[0,"CUP_arifle_M16A2_GL",false],
+[0,"CUP_arifle_M16A4_Base",false],
+[0,"CUP_arifle_M16A4_GL",false],
+
+/* maschinengewehre */
+[0,"CUP_lmg_L7A2",true],
+[0,"LMG_03_F",true],
+[0,"gm_lmgm62_blk",true],
+[0,"gm_mg3_blk",true],
+[0,"gm_mg3_des",true],
+[0,"CUP_lmg_PKM",true],
+[0,"gm_hmgpkm_prp",true],
+[0,"CUP_lmg_UK59",true],
+
+/* schrotgewehre */
+[0,"CUP_sgun_M1014",false]
+
+]
+];
