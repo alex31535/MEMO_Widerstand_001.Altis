ab305183bcdffaf0e48e17384de09264a727f7b4
alex31535 <alex31535@miegelke.de>
2020-11-26 09:22:16 +0100

    Create s_feind_fzg_land_bewaffnet.hpp
    WIP
    liste mit level-abhängigen (bewaffneten) landfahrzeugen

---
+++ s_feind_fzg_land_bewaffnet.hpp
@@ -0,0 +1,20 @@
+s_feind_fzg_land_bewaffnet = [
+
+[/* lvl 0 */
+"CUP_O_Datsun_PK",
+"CUP_O_Datsun_PK_Random",
+"CUP_O_UAZ_MG_CHDKZ",
+"CUP_O_UAZ_MG_SLA",
+"CUP_O_UAZ_MG_CSAT",
+"CUP_O_UAZ_MG_RU",
+"CUP_O_UAZ_MG_TKA"
+],
+[/* lvl 1 */
+/* autos bewaffnet */
+"CUP_O_Hilux_DSHKM_TK_INS",
+"CUP_O_Hilux_DSHKM_CHDKZ",
+"CUP_O_Hilux_DSHKM_OPF_G_F",
+"CUP_O_Hilux_armored_DSHKM_TK_INS",
+"CUP_O_Hilux_armored_DSHKM_OPF_G_F"
+]
+];
