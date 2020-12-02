2116156e350faba5c2ad3447b282ec4a2f258715
alex31535 <alex31535@miegelke.de>
2020-12-02 10:01:30 +0100

    Create s_fzg_land_bewaffnet_resistance.hpp
    WIP
    fahrzeuge der seite "resistance"

---
+++ s_fzg_land_bewaffnet_resistance.hpp
@@ -0,0 +1,20 @@
+s_fzg_land_bewaffnet_resistance = [
+
+[/* lvl 0 */
+"CUP_I_Datsun_PK",
+"CUP_I_Datsun_PK_TK",
+"CUP_I_Datsun_PK_TK_Random",
+"CUP_I_Datsun_PK_Random",
+"CUP_I_UAZ_MG_UN"
+],
+[/* lvl 1 */
+"CUP_I_Hilux_DSHKM_TK",
+"CUP_I_Hilux_DSHKM_IND_G_F",
+"CUP_I_Hilux_DSHKM_NAPA",
+"CUP_I_Hilux_armored_DSHKM_TK",
+"CUP_I_Hilux_armored_DSHKM_IND_G_F",
+"CUP_I_Hilux_armored_DSHKM_NAPA",
+"I_C_Offroad_02_LMG_F",
+"CUP_I_4WD_LMG_ION"
+]
+];
